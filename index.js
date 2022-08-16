0cec6b1a2e798ae9c61a3c2df1f6151d2b718adc
Rodean <rodeanfraser@gmail.com>
2022-08-16 12:28:07 -0400

    node api boilerplate

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
-// const os = require("os");
-// const path = require("path");
-const http = require("http");
-// const { readFileSync, writeFileSync, readFile } = require("fs");
-// const user = os.userInfo();
-// const currentOs = {
-//   name: os.type(),
-//   release: os.release(),
-//   totalMem: os.totalmem(),
-//   freeMem: os.freemem(),
-// };
+require("dotenv").config();
+const express = require("express");
+const server = express();
+server.use(express.json());
 
-// const first = readFileSync("./content/first.txt", "utf8");
-// const second = readFileSync("./content/second.txt", "utf8");
-// writeFileSync(
-//   "./content/results.txt",
-//   `results from writeFileSync is ${first} ${second}`
-//     { flag: "a" }
-// );
+const port = process.env.PORT || 8000;
 
-// const filePath = path.join("/content", "text.txt");
-// const absolute = path.resolve(__dirname, "content", "text.txt");
-
-// readFile("./content/first.txt", "utf8", (err, result) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(result);
-// });
-// console.log(first, second);
-
-const server = http.createServer(async (req, res) => {
-  try {
-    if (req.url === "/") {
-      res.end("Welcome to our app");
-    }
-    if (req.url === "/about") {
-      res.end("Welcome to our about page");
-    }
-    res.end(`
-        <h1>Sorry this page doesn't seem to exist</h1><a href="/">Go back to home page</a>`);
-  } catch (err) {
-    console.log(err);
-  }
+server.listen(port, () => {
+  console.log(`\n --- Server Listening on port ${port}\n`);
 });
-server.listen(5000);
