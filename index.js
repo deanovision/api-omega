d2b048326090049785da6b27ba46fd6fbff22101
Rodean <rodeanfraser@gmail.com>
2022-08-16 12:36:01 -0400

    sanity check

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ server.use(express.json());
 
 const port = process.env.PORT || 8000;
 
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "connected" });
+});
+
 server.listen(port, () => {
   console.log(`\n --- Server Listening on port ${port}\n`);
 });
