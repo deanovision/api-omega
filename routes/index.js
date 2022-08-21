541606edd4f2d8934e224d809121aba130b85d59
Rodean <rodeanfraser@gmail.com>
2022-08-21 15:21:48 -0400

    added new posts seed also a postsmodel and router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const userRouter = require("./userRouter");
+const postsRouter = require("./postsRouter");
 
 module.exports = {
   userRouter,
+  postsRouter,
 };
--- server/serverConfig.js
+++ server/serverConfig.js
@@ -1,7 +1,8 @@
 const { authenticateToken, handleErrors } = require("../middleware");
-const { userRouter } = require("../routes");
+const { userRouter, postsRouter } = require("../routes");
 
 module.exports = (server, router) => {
   server.get("/", (req, res) => res.status(200).json({ message: "connected" }));
   server.use("/users", authenticateToken, userRouter(router), handleErrors);
+  server.use("/posts", authenticateToken, postsRouter(router), handleErrors);
 };
